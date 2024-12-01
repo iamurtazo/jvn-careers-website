9ae994ee1d4abeeb7ce262e0f78b524c65b3936c
murtazo <toshniyozovmurtazo.r538@gmail.com>
2024-12-02 08:01:15 +0900

    Adinco webpage potential outlook

--- app.py
+++ app.py
@@ -2,18 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/home')
+@app.route('/adinco')
 def home():
-    return render_template('home.html')
+    return render_template('adinco.html', active_page='home')
 
+@app.route('/questions')
+def questions():
+    return render_template('questions.html', active_page='questions')
+
+@app.route('/community')
+def community():
+    return render_template('community.html', active_page='community')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', active_page='blog')
+
+@app.route('/ask_question')
+def ask_question():
+    return render_template('ask_question.html', active_page='ask_question')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=False, host='0.0.0.0')
